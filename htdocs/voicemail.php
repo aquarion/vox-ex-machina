6f7aa6b84f711992a80890f73a8d5b7acdc60b1f
Nicholas Avenell <nicholas@istic.net>
2015-10-27 00:46:01 +0000

    Use MP3s for responses, if they're there.

--- htdocs/voicemail.php
+++ htdocs/voicemail.php
@@ -1,8 +1,18 @@
 <?php
+    require "../src/init.php";
     require "../libraries/twilio/Services/Twilio.php";
 
+
+	$contact = search_accounts($ACCOUNTS, $google_api, $_REQUEST['From'], $error);
+
     $response = new Services_Twilio_Twiml();
-    $response->say('You\'ve reached the voicemail for Nicholas Ay-ven-ell, please leave a message');
+    if($contact->found && file_exists("outgoing/".$contact->account.".mp3")){
+	$response->play("http://voicemail.aqxs.net/outgoing/".$contact->account.".mp3");
+    } elseif (file_exists("outgoing/Default.mp3")){
+	$response->play("http://voicemail.aqxs.net/outgoing/Default.mp3");
+    } else {
+    	$response->say('You\'ve reached the voicemail for Nicholas Ay-ven-ell, please leave a message');
+    }
     $response->record(
         array(
             'action' => "handle_message.php",
